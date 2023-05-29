b78d2bdcdfcce97708c897b1ebb0550870f93d74
sivagamasrinivasan <784108@pdsb.net>
2023-05-29 12:25:15 -0400

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3;
+
+/**  Local rep - steps 1. initilize versioning, .gitconfig, .gitignore, 4. commit
+ *
+ * @author srinivsi
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+     System.out.println("learning github operationsljkhlk");
+    }
+    
+}
